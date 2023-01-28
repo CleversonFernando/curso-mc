6b025a2f3161be3d001617d7227ed612d9428dcd
Cleverson Fernando <cfrodrigues50@gmail.com>
2023-01-28 16:18:49 -0300

    Enviando imagem via endpoint

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -20,7 +20,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,6 +35,9 @@ public class ClienteService {
     private EnderecoRepository enderecoRepository;
     @Autowired
     private BCryptPasswordEncoder pe;
+
+    @Autowired
+    private S3Service s3Service;
 
     public  Cliente find(Integer id){
         Optional<Cliente> obj = repo.findById(id);
@@ -84,6 +89,9 @@ public class ClienteService {
     private void updateData(Cliente newObj, Cliente obj) {
         newObj.setNome(obj.getNome());
         newObj.setEmail(obj.getEmail());
+    }
+    public URI uploadProfilePicture(MultipartFile multipartFile){
+        return s3Service.uploadFile(multipartFile);
     }
 }
 
